9c2b5bf6e1d6e39e465879f9d9ac7d4e4a56f36c
5B3susmitha <138358971+5B3susmitha@users.noreply.github.com>
2023-07-02 19:28:54 +0530

    Delete request.py

--- request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'  # Replace with your API endpoint URL
-
-# Make a POST request with the input values
-r = requests.post(url, json={'eur_usd': 1.2, 'spi': 150, 'spx': 3000})
-
-# Print the response
-print(r.json())
